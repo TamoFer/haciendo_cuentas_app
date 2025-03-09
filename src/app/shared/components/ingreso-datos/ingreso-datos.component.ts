6edfaf8bcedec343f8373b9354af06cf2651afc5
Fernando Tamone <104037683+TamoFer@users.noreply.github.com>
2025-03-09 20:34:39 -0300

    form_casi_listo

--- src/app/shared/components/ingreso-datos/ingreso-datos.component.ts
+++ src/app/shared/components/ingreso-datos/ingreso-datos.component.ts
@@ -3,6 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
+
 @Component({
   selector: 'app-ingreso-datos',
   templateUrl: './ingreso-datos.component.html',
@@ -12,18 +13,24 @@ import { IonicModule } from '@ionic/angular';
 })
 export class IngresoDatosComponent implements OnInit {
 
+  contrasenia!: boolean;
+  ocultarVer: boolean = true;
+
   @Input() control!: FormControl;
   @Input() type!: string;
   @Input() label!: string;
   @Input() autocomplete!: string;
   @Input() icon!: string;
-  // @Input() placeholder!: string;
-
 
   constructor() { }
+  ngOnInit() {
+    this.type == 'password' ? this.contrasenia = true : this.contrasenia = false;
+  }
 
+  hideShowPassword() {
+    this.ocultarVer = !this.ocultarVer;
+    this.ocultarVer ? this.type = 'password' : this.type = 'text';
 
-
-  ngOnInit() { }
+  }
 
 }
