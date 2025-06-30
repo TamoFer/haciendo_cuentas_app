a2512d1b612be931ae74933432fc35638be4c9ab
Fernando Tamone <104037683+TamoFer@users.noreply.github.com>
2025-06-29 21:06:03 -0300

    agregando_filtros-gastos

---
+++ src/app/pages/main/consumos/add-update-delete-consumos/add-update-delete-consumos.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-update-delete-consumos',
+  templateUrl: './add-update-delete-consumos.component.html',
+  styleUrls: ['./add-update-delete-consumos.component.scss'],
+})
+export class AddUpdateDeleteConsumosComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/pages/main/gastos/gastos.page.ts
+++ src/app/pages/main/gastos/gastos.page.ts
@@ -1,5 +1,6 @@
 import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { IonicModule, NavController } from '@ionic/angular';
 import { Movimiento } from 'src/app/models/movimiento.mode';
@@ -12,7 +13,7 @@ import { HeaderComponent } from 'src/app/shared/components/header/header.compone
   selector: 'app-gastos',
   templateUrl: './gastos.page.html',
   styleUrls: ['./gastos.page.scss'],
-  imports: [IonicModule, HeaderComponent, FooterComponent, NgIf, NgFor, CommonModule, RouterLink]
+  imports: [IonicModule, HeaderComponent, FooterComponent, NgIf, NgFor, CommonModule, RouterLink, ReactiveFormsModule]
 })
 export class GastosPage implements OnInit {
 
@@ -23,8 +24,24 @@ export class GastosPage implements OnInit {
   movimientosCuenta: Movimiento[] = [];
   usuario = this.utilsSVC.obtenerDatosLS('user');
   totalGastos: string;
+  rubros = ['Compra', 'Regalo', 'Deudas', 'Servicios'];
+  busquedaPorFechas: boolean = false;
+  busquedaPorOtros: boolean = false;
+
+  filtrosFecha = new FormGroup({
+    desde: new FormControl(Validators.required),
+    hasta: new FormControl(Validators.required),
+
+  });
+
+  filtrosOtros = new FormGroup({
+    rubro: new FormControl(Validators.required),
+    detalle: new FormControl(Validators.required),
+
+  });
 
   constructor() { }
+
 
 
 
@@ -41,6 +58,10 @@ export class GastosPage implements OnInit {
 
     });
     this.obtenerMovimientosCuenta();
+  }
+
+  filtrarDatos() {
+
   }
 
 
