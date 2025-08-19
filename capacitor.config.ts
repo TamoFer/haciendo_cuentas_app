e7a5cce3a55f5f6fceb4cae9ecb01ab10524d6bf
Fernando Tamone <104037683+TamoFer@users.noreply.github.com>
2025-08-19 20:42:31 -0300

    mejorar la configuración de la barra de estado y agregar pie de página en las páginas de autenticación y balances

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,14 @@
 import type { CapacitorConfig } from '@capacitor/cli';
+import { StatusBar, Style } from '@capacitor/status-bar';
+
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'haciendo_cuentas_app',
   webDir: 'www'
 };
+
+StatusBar.setOverlaysWebView({ overlay: false });
+StatusBar.setStyle({ style: Style.Dark });
 
 export default config;
